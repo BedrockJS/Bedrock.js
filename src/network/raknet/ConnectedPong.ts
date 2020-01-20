92851b3148e7985195701de721a1fe9a93e3edf8
Dylan Ogle <delxhq@funo.network>
2020-01-20 01:15:09 +0000

    Reverse Magic write

--- src/network/raknet/ConnectedPong.ts
+++ src/network/raknet/ConnectedPong.ts
@@ -18,7 +18,6 @@ export default class ConnectedPong extends EncapsulatedPacket {
     this.getStream()
       .writeLong(this.sendPingTime)
       .writeLong(this.sendPongTime)
-      .writeMagic()
   }
 
 }
